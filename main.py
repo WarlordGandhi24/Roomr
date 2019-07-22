3437259f48bccac2ca7b090eba49efc1dc6d0a1a
Andrew Gan <gans@stanford.edu>
2019-07-22 11:35:39 -0700

    added handler skeleton for the login, profile edit, profile view, and search page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from google.appengine.ext import ndb
+from google.appengine.api import users
 
 import jinja2
 import webapp2
@@ -10,9 +11,42 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def root_parent():
+    return ndb.Key('Parent', 'default_parent')
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/main.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url(self.request.uri),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(data))
+
+class ProfileEditPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/profile_edit.html')
+        self.response.write(template.render())
+
+class ProfileViewPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/profile_view.html')
+        self.response.write(template.render())
+
+class SearchPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/search.html')
+        self.response.write(template.render())
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/delete_dogs', DeleteDogs),
-    ('/cats', CatPage),
-    ('/delete_cats', DeleteCats)
+    ('/profile_edit', ProfileEditPage),
+    ('/profile_view', ProfileViewPage),
+    ('/search', SearchPage)
 ], debug=True)
