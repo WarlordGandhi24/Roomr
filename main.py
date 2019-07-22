b0769d7830b16a66c5c3ecbc898a86f0711987b9
Andrew Gan <gans@stanford.edu>
2019-07-22 15:09:00 -0700

    fixed missing image error for cards, and TRIED to fix the first-time login check issue.

--- main.py
+++ main.py
@@ -61,8 +61,13 @@ class ProfileEditPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user != None:
-            template = JINJA_ENVIRONMENT.get_template('templates/profile_edit.html')
-            self.response.write(template.render())
+            if(User.firsttime == True):
+                template = JINJA_ENVIRONMENT.get_template('templates/profile_edit.html')
+                self.response.write(template.render())
+                User.firsttime = False
+            else:
+                template = JINJA_ENVIRONMENT.get_template('templates/search.html')
+                self.response.write(template.render())
         else:
             self.redirect('/')
     def post(self):
