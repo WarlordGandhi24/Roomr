eaf01816ab517d9540b945411ccae22a2f18b17e
Quinn <QuinnDaCollins@gmail.com>
2019-07-24 10:46:58 -0500

    Basic single user chat functionality added,  next is two users

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,8 +1,12 @@
 const chatbox = document.querySelector("#chatbox")
+
+var count = initialCount
+
 chatbox.addEventListener('keypress', function (key){
   if(key.keycode == 13){
     key.preventDefault()
     document.querySelector("#send").click()
+    document.querySelector("#inputBox").innerHTML = ""
   }
 })
 
@@ -11,23 +15,37 @@ function startTimer() {
   window.setTimeout(fetchUpdatedLog, microseconds)
 }
 
+
 // Ask the server for the current note immediately.
+
 function fetchUpdatedLog() {
+  console.log(count)
+
   fetch('/ajax/get_updated_log')
     .then(function(response) {
+
       return response.json()
     })
     .then(function (myJson) {
-      // Update the div.
-      var newP = document.createElement("p")
-      var newMsg = document.createTextNode(myJson.logs);
-      newP.appendChild(newMsg)
-      chatbox.appendChild(newP)
 
+      // Update the div.
+      console.log(myJson.msgCount)
+
+      if(myJson.msgCount > count){
+      for(i = myJson.msgCount - count; i < myJson.msgCount; i++){
+      newP = document.createElement('p')
+      newP.innerHTML = myJson.msgs[i]
+      chatbox.appendChild(newP)
+      //  initialCount = myJson.msgCount
       // Start the timer again for the next request.
-      startTimer()
+      count = myJson.msgCount
+    }
+
+    }
+    startTimer()
     })
 }
+
 
 if (chatbox != null) {
   // If note_div is null it means that the user is not logged in.  This is
